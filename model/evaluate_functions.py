cddbd62d0d1b4e254c4b8a973ae16ecd9b4be8a4
soravolk <a4586658@gmail.com>
2022-05-21 23:01:50 +0800

    Add YOLOX object detection model and Trim code in evaluate_prediction

--- model/evaluate_functions.py
+++ model/evaluate_functions.py
@@ -15,37 +15,34 @@ from .utils import save_pianoroll
 
 eps = sys.float_info.epsilon    
 
-def evaluate_wo_velocity(data, model, onset_threshold=0.5, frame_threshold=0.5, save_path=None, reconstruction=True, onset=True, pseudo_onset=False, rule='rule2', VAT=False):
-    metrics = defaultdict(list)
+def evaluate_prediction(data, model, save_path=None, reconstruction=True):
+    metrics = defaultdict(list) # a safe dict
     
-    for file in tqdm(data):
-        print('file.shape: ', file['audio'].shape)
-        pred, losses, _ = model.run_on_batch(file, None, False)   
-#         print(f"pred['onset2'] = {pred['onset2'].shape}")
-#         print(f"pred['frame2'] = {pred['frame2'].shape}")            
+    for val_data in tqdm(data):
+        pred, losses, _ = model.run_on_batch(val_data, None, False)
 
         for key, loss in losses.items():
             metrics[key].append(loss.item())
 
         for key, value in pred.items():
             if key in ['technique', 'technique2']:
-                value.squeeze_(0).relu_()
+                value.relu_() # value.shape [232, 10]
+
         # find the technique timings
-        file['technique'] = file['technique'].unsqueeze(1)
-        print("file['technique'].shape: ", file['technique'].shape)
+        # groundtruth: val_data['technique'].shape [232]
+        val_data['technique'] = val_data['technique'] # value.shape [232, 1]
         # get techinique and interval
-        p_ref, i_ref = extract_technique(file['technique'], rule=rule, gt=True)
-        p_est, i_est = extract_technique(pred['technique'], rule=rule)
+        tech_ref, interval_ref = extract_technique(val_data['technique'], gt=True)
+        tech_est, interval_est = extract_technique(pred['technique'])
 
         scaling = HOP_LENGTH / SAMPLE_RATE
 
         # Converting time steps to seconds and midi number to frequency
-        i_ref = (i_ref * scaling).reshape(-1, 2)
-        print("i_ref.shape: ", i_ref.shape)
-        i_est = (i_est * scaling).reshape(-1, 2)
-        print("i_est.shape: ", i_est.shape)
+        interval_ref = (interval_ref * scaling) #[# of techniques, 2(onset, offset)]
+        interval_est = (interval_est * scaling)
 
-        p, r, f, o = evaluate_techniques(i_ref, p_ref, i_est, p_est, offset_ratio=None)
+        # utilize mir_eval(have to shift all technique by 1 because 0 is not allowed)
+        p, r, f, o = evaluate_techniques(interval_ref, tech_ref, interval_est, tech_est, offset_ratio=None)
         print('p: ', p)
         print('r: ', r)
         print('f: ', f)
@@ -54,57 +51,42 @@ def evaluate_wo_velocity(data, model, onset_threshold=0.5, frame_threshold=0.5, 
         metrics['metric/technique/f1'].append(f)
         metrics['metric/technique/overlap'].append(o)     
 
-        p, r, f, o = evaluate_techniques(i_ref, p_ref, i_est, p_est)
+        p, r, f, o = evaluate_techniques(interval_ref, tech_ref, interval_est, tech_est)
+        print('(offset ratio)p: ', p)
+        print('(offset ratio)r: ', r)
+        print('(offset ratio)f: ', f)
         metrics['metric/technique-with-offsets/precision'].append(p)
         metrics['metric/technique-with-offsets/recall'].append(r)
         metrics['metric/technique-with-offsets/f1'].append(f)
         metrics['metric/technique-with-offsets/overlap'].append(o)
-     
-        # frame_metrics = evaluate_frames(t_ref, f_ref, t_est, f_est)
-        # metrics['metric/frame/f1'].append(hmean([frame_metrics['Precision'] + eps, frame_metrics['Recall'] + eps]) - eps)
-
-        ## !!!multiclass format is not supported!!!
-        # avp = average_precision_score(file['technique'].cpu().detach().flatten() ,pred['technique'].cpu().detach().flatten())
-        # metrics['metric/MusicNet/micro_avg_P'].append(avp)     
         
         if reconstruction:
-            p_est2, i_est2 = extract_technique(pred['technique2'], rule=rule)   
-            # t_est2, f_est2 = techniques_to_frames(p_est2, i_est2, pred['frame2'].shape)               
+            tech_est2, interval_est2 = extract_technique(pred['technique2'])                
+            interval_est2 = (interval_est2 * scaling).reshape(-1, 2)  
 
-            i_est2 = (i_est2 * scaling).reshape(-1, 2)
-            # p_est2 = np.array([midi_to_hz(MIN_MIDI + midi) for midi in p_est2])        
-
-            # t_est2 = t_est2.astype(np.float64) * scaling
-            # f_est2 = [np.array([midi_to_hz(MIN_MIDI + midi) for midi in freqs]) for freqs in f_est2]
-
-            p2, r2, f2, o2 = evaluate_techniques(i_ref, p_ref, i_est2, p_est2, offset_ratio=None)
+            p2, r2, f2, o2 = evaluate_techniques(interval_ref, tech_ref, interval_est2, tech_est2, offset_ratio=None)
+            print('recon_p: ', p2)
+            print('recon_r: ', r2)
+            print('recon_f: ', f2)
             metrics['metric/technique/precision_2'].append(p2)
             metrics['metric/technique/recall_2'].append(r2)
             metrics['metric/technique/f1_2'].append(f2)
             metrics['metric/technique/overlap_2'].append(o2)             
-
-            # frame_metrics2 = evaluate_frames(t_ref, f_ref, t_est2, f_est2)
-            # frame_metrics['Precision_2'] = frame_metrics2['Precision']
-            # frame_metrics['Recall_2'] = frame_metrics2['Recall']
-            # frame_metrics['accuracy_2'] = frame_metrics2['Accuracy']
-            # metrics['metric/frame/f1_2'].append(hmean([frame_metrics['Precision_2'] + eps, frame_metrics['Recall_2'] + eps]) - eps)            
-            # avp = average_precision_score(file['technique'].cpu().detach().flatten() ,pred['frame2'].cpu().detach().flatten())
-            # metrics['metric/MusicNet/micro_avg_P2'].append(avp)   
             
-            p2, r2, f2, o2 = evaluate_techniques(i_ref, p_ref, i_est2, p_est2)
+            p2, r2, f2, o2 = evaluate_techniques(interval_ref, tech_ref, interval_est2, tech_est2)
+            print('(offset ratio)recon_p: ', p2)
+            print('(offset ratio)recon_r: ', r2)
+            print('(offset ratio)recon_f: ', f2)
             metrics['metric/technique-with-offsets/precision_2'].append(p2)
             metrics['metric/technique-with-offsets/recall_2'].append(r2)
             metrics['metric/technique-with-offsets/f1_2'].append(f2)
             metrics['metric/technique-with-offsets/overlap_2'].append(o2)             
-            
-        # for key, loss in frame_metrics.items():
-        #     metrics['metric/frame/' + key.lower().replace(' ', '_')].append(loss)
 
         if save_path is not None:
             os.makedirs(save_path, exist_ok=True)
-            file_path = os.path.join(save_path, os.path.basename(file['path']) + '.file.png')
-            save_pianoroll(file_path, file['technique'])
-            pred_path = os.path.join(save_path, os.path.basename(file['path']) + '.pred.png')
+            file_path = os.path.join(save_path, os.path.basename(val_data['path']) + '.file.png')
+            save_pianoroll(file_path, val_data['technique'])
+            pred_path = os.path.join(save_path, os.path.basename(val_data['path']) + '.pred.png')
             save_pianoroll(pred_path, pred['technique'])
     return metrics
 
--- train.py
+++ train.py
@@ -38,16 +38,12 @@ saving_freq = 200
 @ex.config
 def config():
     root = 'runs'
-    # logdir = f'runs_AE/test' + '-' + datetime.now().strftime('%y%m%d-%H%M%S')
-    # Choosing GPU to use
-#     GPU = '0'
-#     os.environ['CUDA_VISIBLE_DEVICES']=str(GPU)
     onset_stack=True
     device = 'cuda:0'
     log = True
     w_size = 31
     spec = 'Mel'
-    resume_iteration = None
+    resume_iteration = None # 'model-1200'
     train_on = 'Solo'
     n_heads=4
     iteration = 10
@@ -88,7 +84,7 @@ def tensorboard_log(batch_visualize, model, valid_set, supervised_loader,
     if (ep)%logging_freq==0 or ep==1:
         # on valid set
         with torch.no_grad():
-            for key, values in evaluate_wo_velocity(valid_set, model, reconstruction=reconstruction, VAT=False).items():
+            for key, values in evaluate_prediction(valid_set, model, reconstruction=reconstruction).items():
                 if key.startswith('metric/'):
                     _, category, name = key.split('/')
                     print(f'{category:>32} {name:25}: {np.mean(values):.3f} ± {np.std(values):.3f}')
@@ -318,24 +314,22 @@ def train(spec, resume_iteration, batch_size, sequence_length, w_size, n_heads, 
     print('batch_visualize_audio.shape: ', batch_visualize['audio'].shape)
 
     ds_ksize, ds_stride = (2,2),(2,2)     
+    model = UNet(ds_ksize,ds_stride, log=log, reconstruction=reconstruction,
+                    mode=mode, spec=spec, device=device, XI=XI, eps=eps)
     if resume_iteration is None:  
-        model = UNet(ds_ksize,ds_stride, log=log, reconstruction=reconstruction,
-                     mode=mode, spec=spec, device=device, XI=XI, eps=eps)
         model.to(device)
         optimizer = torch.optim.Adam(model.parameters(), learning_rate)
         resume_iteration = 0
     else: # Loading checkpoints and continue training
         trained_dir='checkpoint' # Assume that the checkpoint is in this folder
         model_path = os.path.join(trained_dir, f'{resume_iteration}.pt')
-        model = torch.load(model_path)
+        model.load_state_dict(model_path)
         optimizer = torch.optim.Adam(model.parameters(), learning_rate)
         optimizer.load_state_dict(torch.load(os.path.join(trained_dir, 'last-optimizer-state.pt')))
 
     summary(model)
-#     scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=base_lr, max_lr=max_lr, step_size_up=step_size_up,cycle_momentum=False)
+    # scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=base_lr, max_lr=max_lr, step_size_up=step_size_up,cycle_momentum=False)
     scheduler = StepLR(optimizer, step_size=learning_rate_decay_steps, gamma=learning_rate_decay_rate)
-
-    result = pd.DataFrame(columns=['Predicted', 'GroundTruth'])
 
     for ep in tqdm(range(1, epoches+1)):
         if VAT==True:
@@ -366,18 +360,12 @@ def train(spec, resume_iteration, batch_size, sequence_length, w_size, n_heads, 
         for key, value in {**losses}.items():
             writer.add_scalar(key, value.item(), global_step=ep) 
 
-        # Observe a portion of model prediction
-        result = result.append(pd.DataFrame([[predictions['technique'][50:150,:].detach().cpu().numpy(), 
-                                        predictions['technique2'][50:150,:].detach().cpu().numpy(), 
-                                        predictions['annotation'][50:150].cpu().numpy()]],
-                                        columns=['Prediction_1', 'Prediction_2', 'GroundTruth']))
-        result.to_csv('./prediction/result.tsv', sep='\t')
     """
     # Evaluating model performance on the full MAPS songs in the test split     
     print('Training finished, now evaluating on the MAPS test split (full songs)')
     with torch.no_grad():
         model = model.eval()
-        metrics = evaluate_wo_velocity(tqdm(full_validation), model, reconstruction=False,
+        metrics = evaluate_wo_prediction(tqdm(full_validation), model, reconstruction=False,
                                        save_path=os.path.join(logdir,'./MIDI_results'))
         
     for key, values in metrics.items():
